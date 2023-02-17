73fe683fdaf03fcc72e54235012573e7771ad606
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-02-18 08:41:33 +0900

    다른 풀이 추가

--- python/pccp모의고사_유전법칙.py
+++ python/pccp모의고사_유전법칙.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 rs=0
 def back(n,p): #부모세대를 추적해나감. (3,5)->3,4라고하자. 
     global rs
@@ -18,11 +16,6 @@ def back(n,p): #부모세대를 추적해나감. (3,5)->3,4라고하자.
     elif rs=='rr':
         rs=='rr'
     
-    
-
-        
-            
-
 def solution(queries):
     answer = []
     
@@ -32,8 +25,27 @@ def solution(queries):
         back(a,b-1)
         print(rs)
         answer.append(rs)
-        
-        
-        
-    
     return answer
+
+
+#다른 풀이
+"""def back(n,p): 
+    child=['RR','Rr','Rr','rr']
+    if n==1:
+        return 'Rr'
+    parent=back(n-1,p//4)
+    if parent == 'Rr':
+        return child[p%4]
+    else:
+        return parent
+             
+
+def solution(queries):
+    answer = []
+    
+    for i in queries:
+        a,b=i
+        print(a,b)
+        answer.append(back(a,b-1))
+        
+    return answer"""
