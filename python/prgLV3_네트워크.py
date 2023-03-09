c62f66d2e5ae7418005e0980c407a0baf5205734
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-03-09 16:31:06 +0900

    code eff : union-find풀이와 DFS풀이

--- python/prgLV3_네트워크.py
+++ python/prgLV3_네트워크.py
@@ -1,4 +1,4 @@
-#union-find
+#union-find방식의 풀이
 def solution(n, computers):
     answer = 0
     parent=[x for x in range(n)]
@@ -11,7 +11,6 @@ def solution(n, computers):
                 graph[i].append(j)
     print(graph)
     
-    
     def Find(node):
         if parent[node]==node:
             return node
@@ -22,13 +21,9 @@ def solution(n, computers):
         if Find(i)!=Find(j):
             parent[Find(i)]=parent[Find(j)]
             
-    
-    
-    
     for i in range(len(graph)): #[0,1] i는 컴퓨터이름
-        for j in graph[i]:#0 , 1 j는 i랑연결된 컴퓨터 이름
+        for j in graph[i]:  #0 , 1 j는 i랑연결된 컴퓨터 이름
             Union(i,j)
-    
     
     for i in range(len(parent)):
         Find(i)
@@ -38,5 +33,34 @@ def solution(n, computers):
     print(sh)
     answer=len(sh)
                 
-    
     return answer
+
+#DFS로 풀이 : 보다 쉽게 구현 가능
+"""def solution(n, computers):
+    answer = 0
+    chk=[0]*n
+    cnt=0
+    graph=[[] for _ in range(n)]
+    for i in range(n):
+        for j in range(len(computers[i])):
+            if i==j:
+                continue
+            if computers[i][j]==1:
+                graph[i].append(j)
+    def DFS(point):
+        nonlocal cnt,answer
+        for i in point:
+            if chk[i]==0:
+                chk[i]=1
+                DFS(graph[i])
+        
+    groupNum=0
+    for i,point in enumerate(graph):
+        if chk[i]==0:
+            chk[i]=1
+            DFS(point)
+            #여기서 나왔다는건 한줄을 끝까지 이었다는 것.
+            groupNum+=1
+    answer=groupNum
+        
+    return answer"""
