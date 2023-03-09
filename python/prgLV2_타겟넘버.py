b37dc0134a30f89f5e3008e33f716a1dbc916b34
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-03-09 13:59:53 +0900

    code eff : 시간복잡도를 효율적으로 개선

--- python/prgLV2_타겟넘버.py
+++ python/prgLV2_타겟넘버.py
@@ -4,12 +4,12 @@ def solution(numbers, target):
     case=[1,-1]
     nowlist=[]
     def DFS(nowlist,mynum,numbers):
-        print(nowlist)
+        #print(nowlist)
         nonlocal answer
         if len(numbers)==0 and mynum==target:
             answer+=1
             return
-        print('a')
+        #print('a')
         for i in case:
             if len(numbers)!=0:
                 a=numbers[0]
@@ -23,3 +23,30 @@ def solution(numbers, target):
 
 
 print(solution([4, 1, 2, 1],4))
+
+#deque를 활용해 pop과 append의 속도를 O(1)로 만들어 속도를 줄인다. 실제로 최대 941ms의 속도를 840ms로 줄어들었다.
+"""from collections import deque
+def solution(numbers, target):
+    answer = 0
+    case=[1,-1]
+    mysum=0
+    mylist=[]
+    dq=deque(numbers)
+    def DFS(dq,mysum,mylist):
+        nonlocal answer
+        
+        if len(dq)==0 and mysum==target:
+            answer+=1
+            return
+            #print(mysum, answer, mylist)
+        for i in case:
+            if len(dq)!=0:
+                num=dq.popleft()
+                mylist.append(num)
+                DFS(dq,mysum+num*i,mylist)
+                dq.appendleft(num)
+                mylist.pop()
+    DFS(dq,mysum,mylist)
+        
+    
+    return answer"""
