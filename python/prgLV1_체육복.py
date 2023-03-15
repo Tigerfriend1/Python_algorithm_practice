cc4b666057315276caf6f7c290f394c8b7671331
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-03-15 15:31:38 +0900

    code eff : 배열을 이용하여 O(n^2)을 O(n)으로 시간복잡도 개선

--- python/prgLV1_체육복.py
+++ python/prgLV1_체육복.py
@@ -23,3 +23,28 @@ def solution(n, lost, reserve):
         answer+=1
     return answer
 print(solution(13, [1, 2, 5, 6, 10, 12, 13], [2, 3, 4, 5, 7, 8, 9, 10, 11, 12]))
+
+
+#다른 풀이
+"""def solution(n, lost, reserve):
+    answer=0
+    st=[1]*(n+1)
+    #print(st)
+    for i in lost:
+        st[i]-=1
+    for i in reserve:
+        st[i]+=1
+    #print(st)
+    for i in range(1,n+1):
+        if st[i]==0:
+            if i>1 and st[i-1]==2:
+                st[i]+=1
+                st[i-1]-=1
+            elif i!=n and st[i+1]==2:
+                st[i]+=1
+                st[i+1]-=1
+        #print(st)
+    for i in range(1,n+1):
+        if st[i]>0:
+            answer+=1
+    return answer"""
