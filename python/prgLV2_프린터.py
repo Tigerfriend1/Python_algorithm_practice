3e95f3b606eb12663461d465fc03a37706f3945f
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-03-17 01:05:23 +0900

    additional solution

--- python/prgLV2_프린터.py
+++ python/prgLV2_프린터.py
@@ -21,3 +21,27 @@ def solution(priorities, location):
         cnt+=1
         del maxq[0]
         
+
+
+#보다 직관적인 풀이
+"""from collections import deque
+def solution(priorities, location):
+    new=[]
+    for i,val in enumerate(priorities):
+        new.append((i,val))
+    dq=deque(new)
+    cnt=0
+    while dq:
+        flag=0
+        now=dq.popleft()
+        for i in range(len(dq)):
+            if dq[i][1]>now[1]:
+                dq.append(now)
+                flag=1
+                break
+        if flag==1:
+            continue
+        elif flag==0 and now[0]==location:
+            return cnt+1
+        else:
+            cnt+=1"""
