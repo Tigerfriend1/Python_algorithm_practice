f8316374681b483c243145e6dd9f85fe092ceedb
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-03-17 01:39:52 +0900

    resolve

--- python/prgLV2_다리를지나는트럭.py
+++ python/prgLV2_다리를지나는트럭.py
@@ -17,3 +17,26 @@ def solution(bl, w, tw):
         #print(time,Bl,dq)
         
     return time+bl
+
+#다시 풀기
+"""from collections import deque
+def solution(bridge_length, weight, truck_weights):
+    dq=deque(truck_weights) #대기트럭
+    nowb=deque([0]*bridge_length) #다리를 건너는 트럭
+    nowW=0
+    cnt=0
+    while len(dq)!=0:
+        a=nowb.popleft()
+        nowW-=a
+        now=dq.popleft()
+        if nowW+now>weight:
+            nowb.append(0)
+            dq.appendleft(now)
+        else:
+            nowb.append(now)
+            nowW+=now
+        
+        cnt+=1
+        #print(nowb,nowW,now)
+    
+    return cnt+bridge_length"""
