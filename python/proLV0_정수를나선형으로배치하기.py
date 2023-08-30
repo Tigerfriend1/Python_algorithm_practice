a9564863cd0f15c4933344db63b028f33be5b9a6
kimtg1997 <kimtg1997@pusan.ac.kr>
2023-08-30 16:23:17 +0900

    new solve: 2by2

---
+++ python/proLV0_정수를나선형으로배치하기.py
@@ -0,0 +1,23 @@
+def solution(n):
+    answer = [[0] * n for _ in range(n)]
+    dy = [0, 1, 0, -1]
+    dx = [1, 0, -1, 0]
+
+    y, x = 0, 0
+    num = 1
+    direction = 0
+    
+    while num<=n*n:
+        answer[y][x]=num
+        num+=1
+        ny=y+dy[direction]
+        nx=x+dx[direction]
+        if nx>n-1 or ny>n-1 or nx<0 or ny<0 or answer[ny][nx]!=0:
+            direction=(direction+1)%4
+            ny=y+dy[direction]
+            nx=x+dx[direction]
+        y=ny
+        x=nx
+    
+
+    return answer
